01bf3549deedcae2ef8c44ba85e57bd2653885e6
Don Park <donp@donp.org>
2019-11-13 12:13:18 -0800

    test bits and skip jq docs

--- libjq.nimble
+++ libjq.nimble
@@ -13,4 +13,5 @@ requires "nim >= 1.0.0"
 task test, "run test app":
   if not dirExists("jq"):
     exec "git clone --depth 1 --recurse-submodules https://github.com/stedolan/jq"
-    exec "cd jq && autoreconf -fi && ./configure && make"
+    exec "cd jq && autoreconf -fi && ./configure && make libjq.la"
+  exec "nim c tests/testlibjq.nim"
--- tests/testlibjq.nim
+++ tests/testlibjq.nim
@@ -5,3 +5,20 @@ test "diag":
   let json = "[]"
   var jq_state = libjq.jq_init()
   check true == true
+
+test "jq object":
+  var jqstate = jq_init()
+  var jq_int = jq_compile(jqstate, ".")
+  jq_start(jqstate, jv_object(), 0)
+  var jvalue = jq_next(jq_state)
+  var jqk = jv_get_kind(jvalue)
+  assert jqk == JV_KIND_OBJECT
+
+test "jq array":
+  var jqstate = jq_init()
+  var jq_int = jq_compile(jqstate, "keys")
+  var input = jv_parse("{}")
+  jq_start(jqstate, input, 0)
+  var jvalue = jq_next(jq_state)
+  var jqk = jv_get_kind(jvalue)
+  assert jqk == JV_KIND_ARRAY

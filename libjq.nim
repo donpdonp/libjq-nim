b8216f26242ee2dd3c88af0cc7e1a43a2027be6e
Don P <donp@donp.org>
2020-02-10 11:29:37 -0800

    jq_object_get

--- libjq.nim
+++ libjq.nim
@@ -13,9 +13,11 @@ proc jq_start*(jsq: jq_State, jv: jq_Value, flags: cint) {.header: "<jq.h>", imp
 proc jq_compile*(jsq: jq_State, jp: cstring): int {.header: "<jq.h>", importc.}
 proc jq_next*(jsq: jq_State): jq_Value {.header: "<jq.h>", importc.}
 proc jv_get_kind*(jv: jq_Value): jv_Kind {.header: "<jq.h>", importc.}
+proc jv_object_get*(jv: jq_Value, key: jq_Value): jq_Value {.header: "<jq.h>", importc.}
 proc jv_array_length*(jv: jq_Value): cint {.header: "<jq.h>", importc.}
 proc jv_array_get*(jv: jq_Value, idx: cint): jq_Value {.header: "<jq.h>", importc.}
 proc jv_object*(): jq_Value {.header: "<jq.h>", importc.}
+proc jv_string*(str: cstring): jq_Value {.header: "<jq.h>", importc.}
 proc jv_parse*(str: cstring): jq_Value {.header: "<jq.h>", importc.}
 proc jv_number_value*(jv: jq_Value): cdouble {.header: "<jq.h>", importc.}
 proc jv_string_value*(jv: jq_Value): cstring {.header: "<jq.h>", importc.}
--- tests/testlibjq.nim
+++ tests/testlibjq.nim
@@ -14,6 +14,15 @@ test "jq object":
   var jqk = jv_get_kind(jvalue)
   assert jqk == JV_KIND_OBJECT
 
+test "jq object get":
+  var jqstate = jq_init()
+  var jq_int = jq_compile(jqstate, "{\"key\":\"value\"")
+  jq_start(jqstate, jv_object(), 0)
+  var jvalue = jq_next(jq_state)
+  var jqv = jv_object_get(jvalue, jv_string("key"))
+  var v = jv_string_value(jqv)
+  check v == "value"
+
 test "jq array":
   var jqstate = jq_init()
   var jq_int = jq_compile(jqstate, "keys")
